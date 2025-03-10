f5d173eaa8ed717581ac175bc75067718afb1c3a
Ade <joelayomide35@gmail.com>
2025-03-10 23:19:08 +0100

    responsiveness

--- src/pages/LandingPage/FindYourPlace.tsx
+++ src/pages/LandingPage/FindYourPlace.tsx
@@ -67,15 +67,7 @@ const FindYourPlace = () => {
 
     return (
         <section ref={ref} className="py-16 px-4 md:px-6 lg:px-8 bg-cream relative">
-            <motion.div
-                className="absolute top-10 left-10 text-purple-600 text-2xl"
-                variants={starVariants}
-                initial="hidden"
-                animate={isInView ? "visible" : "hidden"}
-                custom={1}
-            >
-
-            </motion.div>
+          
 
             <motion.div
                 className="absolute bottom-10 right-10 w-6 h-6 bg-yellow-400 rounded-sm rotate-45"
@@ -84,7 +76,7 @@ const FindYourPlace = () => {
                 animate={isInView ? "visible" : "hidden"}
                 custom={2}
             />
-            <div className="absolute flex justify-between w-full mt-10 right-0">
+            <div className="absolute flex justify-between w-full -mt-10 lg:mt-10 right-0">
                 <Star classes="52" style="" />
                 <div className="absolute right-20"><svg width="55" height="49" viewBox="0 0 55 49" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M51.183 21.4372C52.8897 23.0232 52.8851 25.7264 51.1732 27.3066L41.7135 36.0384L32.9501 45.4687C31.3641 47.1754 28.6609 47.1709 27.0807 45.4589L18.3489 35.9992L8.91859 27.2358C7.21192 25.6499 7.21645 22.9467 8.92843 21.3664L18.3881 12.6347L27.1515 3.20434C28.7375 1.49767 31.4406 1.50219 33.0209 3.21417L41.7527 12.6738L51.183 21.4372Z" fill="black" />
