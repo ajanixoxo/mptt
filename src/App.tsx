b0301d4fd97b723bf9663aea0151607b5700d849
Ade <joelayomide35@gmail.com>
2025-03-14 19:16:46 +0100

    Fixing

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import { useState, useEffect } from "react"
 import { AnimatePresence } from "framer-motion"
 import LoadingScreen from "./components/LoadingScreen"
-import { Routes, Route } from 'react-router'
+import { Routes, Route } from 'react-router-dom'
 import Landing_Page from './pages/Landing_Page/Landing_Page'
 import About from './pages/About/About'
 import Program from './pages/Program/Program'
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useEffect } from "react"
-import { Link, useLocation } from "react-router"
+import { Link, useLocation } from "react-router-dom"
 import { motion } from "framer-motion"
 import { Menu, X } from "lucide-react"
 import Logo from "/Logow.png"
