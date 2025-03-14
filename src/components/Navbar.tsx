1179e4a68cee49fbf9d38a04638290bfaf733dc8
Ade <joelayomide35@gmail.com>
2025-03-14 19:16:01 +0100

    Trying to fix routing issue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,7 +28,7 @@ const Navbar = () => {
   return (
     <motion.header
       className={`fixed top-0 left-0 right-0 z-50 py-4 px-6 transition-all duration-300 ${
-        scrolled ? "b/95 backdrop-blur-sm !text-black shadow-sm" : "bg-transparent"
+        scrolled ? "bg-[#0e032ccc] backdrop-blur-sm !text-black shadow-sm" : "bg-transparent"
       }`}
       initial={{ y: -100 }}
       animate={{ y: 0 }}
