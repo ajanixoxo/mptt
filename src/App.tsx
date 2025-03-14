f25445d57d364c55b7fae8c9e2061c9153036525
Ade <joelayomide35@gmail.com>
2025-03-14 21:12:02 +0100

    Works well brvh

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import { useState, useEffect } from "react"
 import { AnimatePresence } from "framer-motion"
 import LoadingScreen from "./components/LoadingScreen"
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Landing_Page from './pages/Landing_Page/Landing_Page'
 import About from './pages/About/About'
 import Program from './pages/Program/Program'
@@ -21,30 +21,15 @@ function App() {
 
   return (
     <>
-      
-      <Router >
-        <AnimatePresence>{isLoading && <LoadingScreen />}</AnimatePresence>
+      <AnimatePresence>{isLoading && <LoadingScreen />}</AnimatePresence>
+
       <Navbar />
-        <Routes>
-          <Route path="/" element={
-
-
-            <Landing_Page />
-
-          } />
-          <Route path="about" element={
-
-            <About />
-
-          } />
-          <Route path="program" element={
-
-            <Program />
-
-          } />
-        </Routes>
-      </Router>
-
+      <Routes>
+        <Route path="/" element={  <Landing_Page />} />
+        <Route path="about" element={ <About /> } />
+        <Route path="program" element={  <Program />
+        } />
+      </Routes>
     </>
   )
 }
