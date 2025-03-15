4e040750302dade2f6ca1be8f4f2bc07a721f403
Ade <joelayomide35@gmail.com>
2025-03-15 22:53:21 +0100

    Fixed

--- v1/src/App.tsx
+++ v1/src/App.tsx
@@ -1,27 +1,27 @@
 import Navbar from './components/Navbar'
 import './App.css'
-import { useState, useEffect } from "react"
-import { AnimatePresence } from "framer-motion"
-import LoadingScreen from "./components/LoadingScreen"
+// import { useState, useEffect } from "react"
+// import { AnimatePresence } from "framer-motion"
+// import LoadingScreen from "./components/LoadingScreen"
 import { Routes, Route } from 'react-router-dom'
 import Landing_Page from './pages/Landing_Page/Landing_Page'
 import About from './pages/About/About'
 import Program from './pages/Program/Program'
 function App() {
-  const [isLoading, setIsLoading] = useState(true)
+  // const [isLoading, setIsLoading] = useState(true)
 
-  useEffect(() => {
-    // Simulate loading time
-    const timer = setTimeout(() => {
-      setIsLoading(false)
-    }, 2000)
+  // useEffect(() => {
+  //   // Simulate loading time
+  //   const timer = setTimeout(() => {
+  //     setIsLoading(false)
+  //   }, 2000)
 
-    return () => clearTimeout(timer)
-  }, [])
+  //   return () => clearTimeout(timer)
+  // }, [])
 
   return (
     <>
-      <AnimatePresence>{isLoading && <LoadingScreen />}</AnimatePresence>
+      {/* <AnimatePresence>{isLoading && <LoadingScreen />}</AnimatePresence> */}
 
       <Navbar />
       <Routes>
