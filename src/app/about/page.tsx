8801a37eb43d9a58a26b9e6b043cd85ff606a954
Ade <joelayomide35@gmail.com>
2025-03-17 23:56:37 +0100

    fIXED NUILD ERRSO

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,32 +7,33 @@ import ProblemSection from "@/components/about/TheProblem"
 import MissionSection from "@/components/about/TheMission"
 import PartnersSection from "@/components/about/Partners"
 import WhyChooseSection from "@/components/about/HeroSection"
-
+import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
 
 const About = () => {
 
 
   return (
     <>
- <Navbar />
-     <main className="bg-[#0A0A0B] min-h-screen ">
-            {/* Grid Background */}
-            {/* <div className="fixed inset-0 z-0">
+      <Navbar />
+      <main className="bg-[#0A0A0B] min-h-screen ">
+        {/* Grid Background */}
+        {/* <div className="fixed inset-0 z-0">
               <div className="absolute inset-0 bg-grid-white/[0.02] bg-[length:50px_50px]" />
               <div className="absolute inset-0 bg-gradient-to-t from-[#0A0A0B] via-transparent to-transparent" />
             </div> */}
-    
-            {/* Content */}
-            <div className="relative z-10">
-              <WhyChooseSection />
-              <ProblemSection />
-              <MissionSection />
-              <VisionSection />
-              <TeamSection />
-              <PartnersSection />
-      <Footer />
-            </div>
-          </main>
+
+        {/* Content */}
+        <div className="relative z-10">
+          <WhyChooseSection />
+          <ProblemSection />
+          <MissionSection />
+          <VisionSection />
+          <TeamSection />
+          <PartnersSection />
+          <Footer />
+        </div>
+      </main>
     </>
   )
 }
