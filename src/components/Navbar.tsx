5dea52cdfefa4d2d1992bdc8de96d3512f97277b
Saka <tech@mypath2tech.ca>
2025-03-22 10:32:55 +0100

    fixed text color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,7 +29,7 @@ const Navbar = () => {
   return (
     <motion.header
       className={`fixed top-0 left-0 right-0 z-50  py-4 px-6 transition-all duration-300 ${
-        scrolled ? "bg-transparent backdrop-blur-sm !text-black shadow-sm" : "bg-transparent"
+        scrolled ? "bg-transparent backdrop-blur-sm !text-white shadow-sm" : "bg-transparent"
       }`}
       initial={{ y: -100 }}
       animate={{ y: 0 }}
