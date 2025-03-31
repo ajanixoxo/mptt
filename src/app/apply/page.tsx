543a2db589911e837147f24535bd4d32277e4e06
Saka <tech@mypath2tech.ca>
2025-04-01 00:53:34 +0100

    fixed some to responsiveness

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -1,20 +1,18 @@
-"use client"
-import { useEffect, useState, useRef } from "react"; 
-import LoadingScreen from "@/components/LoadingScreen"; 
-// import { usePathname, useRouter } from 'next/navigation' 
-// import { motion } from "framer-motion" 
-// import { Menu, X, ChevronRight, CircleArrowUp } from "lucide-react"
-// import Link from "next/link"
+"use client";
+
+import { useEffect, useState, useRef } from "react";
+import LoadingScreen from "@/components/LoadingScreen";
+import Navbar from "@/components/Navbar";
 
 function Apply() {
-  const [isLoading, setIsLoading] = useState(true); const formRef = useRef<HTMLDivElement>(null); 
-  // const [isOpen, setIsOpen] = useState(false) 
-  // const [scrolled, setScrolled] = useState(false) 
-  // const location = usePathname() 
-  // const router = useRouter()
+  const [isLoading, setIsLoading] = useState(true);
+  const formRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const script = document.createElement("script"); script.src = "//embed.typeform.com/next/embed.js"; script.async = true; document.body.appendChild(script);
+    const script = document.createElement("script");
+    script.src = "//embed.typeform.com/next/embed.js";
+    script.async = true;
+    document.body.appendChild(script);
 
     const observer = new MutationObserver((mutations) => {
       mutations.forEach((mutation) => {
@@ -23,7 +21,6 @@ function Apply() {
           (mutation.target as HTMLElement).getAttribute("data-tf-loaded") === "true"
         ) {
           setIsLoading(false);
-          document.body.style.overflow = "hidden"; // Prevents scrolling when Typeform loads
         }
       });
     });
@@ -33,131 +30,22 @@ function Apply() {
     }
 
     return () => {
-      document.body.style.overflow = "auto"; // Restore scrolling when leaving
       document.body.removeChild(script);
       observer.disconnect();
     };
   }, []);
 
-//   const handleClick = () => { router.push('/apply') } // Close mobile menu when route changes 
-//  useEffect(() => { setIsOpen(false) }, [location])
-
-//   useEffect(() => { const handleScroll = () => { setScrolled(window.scrollY > 10) } 
-//   window.addEventListener("scroll", handleScroll) 
-//   return () => window.removeEventListener("scroll", handleScroll) },
-//    [])
-
   return (
-  <div className="relative flex flex-col overflow-y-auto h-screen">
-     {isLoading && <LoadingScreen />}
-
-    {/* <motion.header
-      className={`fixed top-3 left-0 right-0 z-50 flex items-center justify-center transition-all duration-300 ${scrolled ? "bg-transparent backdrop-blur-sm !text-white shadow-sm" : "bg-transparent"
-        }`}
-      initial={{ y: 0 }}
-      animate={{ y: 0 }}
-      transition={{ duration: 0.5 }}
-    >
-      <div className="container mt-4  mx-auto flex justify-between w-[90%] items-center">
-        <Link href="/" className="flex items-center justify-center ml-4 gap-2">
-          <img src="/Logo.png" alt="logo" className="w-16 md:w-22" />
-        
-        </Link>
-
+    <div className="">
+      {isLoading && <LoadingScreen />}
       
-        <nav className="hidden md:flex items-center gap-5 space-x-8 sec_text">
-          <Link
-            href="/"
-            className={`${location === "/" ? "text-white font-semibold" : "text-gray-800"} flex items-center hover:text-gray300 transition-colors`}
-          >
-            {`${location}` === "/" ? <ChevronRight className="w-auto lg:w-5 h-auto" /> : ""} Home
-          </Link>
-          <Link
-            href="/about"
-            className={`${location === "/about" ? "text-white font-semibold" : "text-gray-800"} flex items-center hover:text-gray300 transition-colors`}
-          >
-            {`${location}` === "/about" ? <ChevronRight className="w-auto lg:w-5 h-auto" /> : ""}  About
-          </Link>
-          <Link
-            href="/program"
-            className={`${location === "/program" ? "text-white font-semibold" : "text-gray-800"} flex items-center hover:text-gray300  transition-colors`}
-          >
-            {`${location}` === "/program" ? <ChevronRight className="w-auto lg:w-5 h-auto" /> : ""}  Program
-          </Link>
-        </nav>
+      
 
-        <div className="hidden md:block">
-          <motion.button
-            className={`  bg-[#DED6E8]  cursor-pointer button text-black px-10 rounded-xl flex items-center justify-center space-x-2 w-[130px] h-10`}
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-            onClick={handleClick}
-          >
-            <span className="font-normal">Apply Now</span>
-            <CircleArrowUp className="rotate-45" />
-          </motion.button>
-        </div>
-
-     
-        <button
-          className="md:hidden "
-          onClick={() => setIsOpen(!isOpen)}
-          aria-label={isOpen ? "Close menu" : "Open menu"}
-        >
-          {isOpen ? <X size={24} className="text-black" /> : <Menu size={24} className="text-black" />}
-        </button>
+      {/* Main Content */}
+      <div className="flex-grow flex items-center justify-center">
+        <div ref={formRef} data-tf-live="01JQ5GSABRY0SKWCX7Q0TNJBM0" className="w-full max-w-4xl p-4" />
       </div>
-
-     
-      {isOpen && (
-        <motion.div
-          className="md:hidden  h-[200px] absolute bg-cream top-full left-0 right-0  shadow-md py-4 px-6"
-          initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -20 }}
-          transition={{ duration: 0.3 }}
-        >
-          <nav className="flex flex-col   gap-5 space-y-4 sec_text">
-            <Link
-              href="/"
-              className={`${location === "/" ? "text-white font-semibold" : "text-gray-100"} hover:text-purple-600 transition-colors py-2`}
-            >
-              Home
-            </Link>
-            <Link
-              href="/about"
-              className={`${location === "/about" ? "text-white font-semibold" : "text-gray-100"} hover:text-purple-600 transition-colors py-2`}
-            >
-              About
-            </Link>
-            <Link
-              href="/program"
-              className={`${location === "/program" ? "text-white font-semibold" : "text-gray-100"} hover:text-purple-600 transition-colors py-2`}
-            >
-              Program
-            </Link>
-            <motion.div className="w-full" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
-              <motion.button
-                className={`  bg-[#DED6E8]  cursor-pointer button text-black rounded-xl flex items-center justify-center space-x-2 w-[130px] h-10`}
-                whileHover={{ scale: 1.05 }}
-                whileTap={{ scale: 0.95 }}
-                onClick={handleClick}
-              >
-                <span className="font-normal">Apply Now</span>
-                <CircleArrowUp className="rotate-45" />
-              </motion.button>
-            </motion.div>
-          </nav>
-        </motion.div>
-      )}
-    </motion.header> */}
-
-    {/* Main Content */}
-    <div className="flex-grow relative z-10 flex items-center justify-center overflow-scroll">
-
-      <div ref={formRef} data-tf-live="01JQ5GSABRY0SKWCX7Q0TNJBM0" className="w-full max-w-4xl p-4" />
     </div>
-  </div>
   );
 }
 
