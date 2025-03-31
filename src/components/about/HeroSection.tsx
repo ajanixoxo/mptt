9642a49dc55c0b821a14f8dced53ac5cdaf5fe38
Mypath2tech-admin <tech@mypath2tech.ca>
2025-03-31 02:36:15 +0000

    heading word changing

--- src/components/about/HeroSection.tsx
+++ src/components/about/HeroSection.tsx
@@ -147,7 +147,7 @@ const WhyChooseSection = () => {
 
             <h1 className="main_text text-center md:text-left text-5xl md:text-7xl font-bold  text-white">
               One <span className="text-[#F9C23A]">Aim.</span><br/>
-              Secure <span className="text-[#F9C23A]">Internships.</span>
+              Equip the <span className="text-[#F9C23A]">Next Gen.</span>
             </h1>
             <p className="text-gray-400 sec_text text-center md:text-left text-lg mb-8 max-w-lg">
             We connect underrepresented youth with the resources (crucial information, hands-on practices and workshops) to land real tech internships and launch real futures.
